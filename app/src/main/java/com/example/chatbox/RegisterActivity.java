046a3d31db7d910ac5cdd77fe553eb3133485813
smita-code <smitakundu111@gmail.com>
2019-11-26 23:15:14 +0600

    test

--- app/src/main/java/com/example/chatbox/RegisterActivity.java
+++ app/src/main/java/com/example/chatbox/RegisterActivity.java
@@ -3,16 +3,12 @@ package com.example.chatbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_register);
     }
 }
